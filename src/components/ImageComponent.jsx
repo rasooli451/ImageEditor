f3fc3fc043fd58083dd06d7da3f430a083a309a5
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2026-04-04 00:38:33 -0400

    added links between this component and App.jsx to facilitate resizing and croping

--- src/components/ImageComponent.jsx
+++ src/components/ImageComponent.jsx
@@ -6,12 +6,34 @@ import Tools from "./Tools"
 import ReactCrop from "react-image-crop";
 import "react-image-crop/dist/ReactCrop.css";
 
-export default function ImageComponent({image,file, selectable, triggerSelect}){
+export default function ImageComponent({image,file, selectable, triggerSelect, formSelection, cropFunc}){
 
-    const [crop, setCrop] = useState();
+    const [crop, setCrop] = useState(null);
+    const [displayApplyCropButton, setDisplayApplyCropButton] = useState(false);
+
+
+
+    function tempFunc(){
+        triggerSelect();
+        setDisplayApplyCropButton(!displayApplyCropButton);
+    }
+
+    function passUpFormSelection(identity){
+        formSelection(identity);
+    }
+
+
+    function applyCrop(){
+        if (crop == null){
+            console.log("empty crop");
+        }
+        else{
+            cropFunc(crop);
+        }
+    }
 
     return <div className="display">
-        <Tools file={file} switchSelect={triggerSelect}/>
+        <Tools switchSelect={tempFunc} unimportantfunc={(identity)=> passUpFormSelection(identity)} />
         <div className="ImageContainer">
             { selectable ? 
             <ReactCrop crop={crop} onChange={(c) => setCrop(c)}>
@@ -21,6 +43,7 @@ export default function ImageComponent({image,file, selectable, triggerSelect}){
             <img src={image} />
             </div> 
             }
+            {displayApplyCropButton ? <button onClick={applyCrop}>Crop</button> : null}
         </div>
     </div> 
 }

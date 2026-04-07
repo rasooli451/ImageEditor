e7d5ed94dc5243b40a8c37c0a1ec232b3169e787
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2026-04-07 04:29:35 -0400

    crop working, need to handle edge cases

--- src/components/ImageComponent.jsx
+++ src/components/ImageComponent.jsx
@@ -10,6 +10,8 @@ export default function ImageComponent({image,file, selectable, triggerSelect, f
 
     const [crop, setCrop] = useState(null);
     const [displayApplyCropButton, setDisplayApplyCropButton] = useState(false);
+    const [naturalImageMeta, setNaturalImageMeta] = useState(null);
+    const [displayImageMeta, setDisplayImageMeta] = useState(null);
 
 
 
@@ -24,11 +26,18 @@ export default function ImageComponent({image,file, selectable, triggerSelect, f
 
 
     function applyCrop(){
-        if (crop == null){
+        if (crop == null || naturalImageMeta == null || displayImageMeta == null){
             console.log("empty crop");
         }
         else{
-            cropFunc(crop);
+             const scaleX = naturalImageMeta.width / displayImageMeta.width;
+             const scaleY = naturalImageMeta.height / displayImageMeta.height;
+             const finalC = {x : Math.round(crop.x * scaleX), y: Math.round(crop.y * scaleY), width : Math.round(crop.width * scaleX), height : Math.round(crop.height * scaleY)};
+             console.log(finalC);
+             cropFunc(finalC);
+             setCrop(null);
+             setDisplayApplyCropButton(false);
+             triggerSelect(); 
         }
     }
 
@@ -36,8 +45,13 @@ export default function ImageComponent({image,file, selectable, triggerSelect, f
         <Tools switchSelect={tempFunc} unimportantfunc={(identity)=> passUpFormSelection(identity)} />
         <div className="ImageContainer">
             { selectable ? 
-            <ReactCrop crop={crop} onChange={(c) => setCrop(c)}>
-                 <img src={image} alt="To crop" />
+            <ReactCrop crop={crop} onChange={(c) =>setCrop(c)} unit="px" key={image}>
+                 <img src={image} alt="To crop" onLoad={(e)=> {
+                    const img = e.target;
+                    setNaturalImageMeta({width : img.naturalWidth, height : img.naturalHeight});
+                    const rect = img.getBoundingClientRect();
+                    setDisplayImageMeta({width : rect.width, height: rect.height});
+                 }}/>
             </ReactCrop>: 
             image == null ? <h2>Upload an image to display</h2>: <div>
             <img src={image} />

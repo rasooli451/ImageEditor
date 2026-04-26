cf9e8c863bd424b9733cdd64ad13996aa6deca94
Farhad Rasoli <rasoolyfarhad7@gmail.com>
2026-04-26 15:04:14 -0400

    removed unnecessary buttons

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -20,7 +20,6 @@ export default function Tools({switchSelect, unimportantfunc, Compress, removeBG
         <button className="button" type="button"onClick={toggleForm} data-identity="Convert">Convert</button>
         <button className="button" type="button" onClick={Compress}>Compress</button>
         <button className="button" type="button" onClick={toggleForm} data-identity="Thumbnail" >Thumbnail</button>
-        <button className="button" type="button">Filters & Effects</button>
         <button className="button" type="button" onClick={removeBG}>Remove Background</button>
     </div>
 }
